06615acd461233540f29e6ca98f8e27854352ccd
Colin Herbst <colinherbst15@gmail.com>
2022-03-06 23:52:58 -0600

    CAC-102 Created Exercise 3

---
+++ exercise3.py
@@ -0,0 +1,29 @@
+# 2-3 Personal Message
+
+name = 'colin'
+print(f'Hello {name.title()}, would you like to learn some Python today?')
+
+# 2-4 Name Cases
+
+name = 'Colin Herbst'
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+# 2-5 Famous Quote
+
+print('Michael Jordan once said, “No bird soars too high, if he soars with his own wings.”')
+
+# 2-6 Famous Quote 2
+
+famous_person = 'Michael Jordan'
+message = ' once said, “No bird soars too high, if he soars with his own wings.”'
+print(famous_person + message)
+
+# 2-7 Stripping Names
+
+name = ' colin '
+print(name)
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
