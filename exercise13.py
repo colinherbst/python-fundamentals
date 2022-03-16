c80b6939ce16344b9d2aba0fa0b443e3d1d2b78e
Colin Herbst <colinherbst15@gmail.com>
2022-03-16 11:38:09 -0500

    CAC-102 Created Exercise 13

---
+++ exercise13.py
@@ -0,0 +1,15 @@
+# 8-15
+import printing_functions as pf
+
+unprinted_designs = ['ball', 'phone stand', 'coaster']
+completed_models = []
+
+pf.print_models(unprinted_designs, completed_models)
+pf.show_completed_models(completed_models)
+
+# 9-10 through 9-12
+from restaurant import Restaurant
+
+zaxbys = Restaurant('zaxbys', 'chicken')
+zaxbys.describe_restaurant()
+zaxbys.open_restaurant()
---
+++ restaurant.py
@@ -0,0 +1,19 @@
+class Restaurant:
+    def __init__(self, name, cuisine_type):
+        self.name = name.title()
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        msg = self.name + " serves " + self.cuisine_type + "."
+        print("\n" + msg)
+
+    def open_restaurant(self):
+        msg = self.name + " is open."
+        print("\n" + msg)
+
+    def set_number_served(self, number_served):
+        self.number_served = number_served
+
+    def increment_number_served(self, additional_served):
+        self.number_served += additional_served
